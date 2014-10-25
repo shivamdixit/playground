c2488b288d9b1b24e546e202753383dd17cb7808
Shivam Dixit <shivamd001@gmail.com>
2014-10-25 11:11:11 +0530

    Added price calculator
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

---
+++ test/PriceCalculator.java
@@ -0,0 +1,14 @@
+/**
+ * class PriceCalculator
+ *
+ * class to calculate price of an order and store it
+ */
+class PriceCalculator
+{
+    public static void run(Order o)
+    {
+        // Business logic to calculate price
+        o.setPrice(1000);   // Some random value
+        // Set the price of the object here only
+    }
+}
